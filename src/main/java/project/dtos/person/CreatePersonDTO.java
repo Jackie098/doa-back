b7e3642c74976887f7841321339a4daf6487c2c3
Carlos Augustoo <carlos.aug.developer@gmail.com>
2025-05-29 19:07:40 -0300

    feat: added validation to person dto

--- src/main/java/project/dtos/person/CreatePersonDTO.java
+++ src/main/java/project/dtos/person/CreatePersonDTO.java
@@ -1,5 +1,7 @@
 package project.dtos.person;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +10,16 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CreatePersonDTO {
+  @Email
+  @NotBlank
   private String email;
+
+  @NotBlank
   private String name;
+
+  @NotBlank
   private String document;
+
+  @NotBlank
   private String phoneNumber;
 }
